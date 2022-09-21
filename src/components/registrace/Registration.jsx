99dc9ad1f05c23d7e48a8a1ee709824746aebf1f
Miriam <rezakmiriam@gmail.com>
2022-09-21 23:50:03 +0200

    logika-1.verze

---
+++ src/components/registrace/Registration.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+
+const Registration = () => {
+  const [user, setUser] = useState({
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  });
+
+  //console.log({ user });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(user);
+  };
+
+  const handleChange = (event) => {
+    setUser({
+      ...user,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleChangeUsername = (event) => {
+    setUser({
+      ...user,
+      email: event.target.value,
+    });
+    console.log(email.value);
+    console.log(user.email);
+    console.log(username.value);
+    //changeUsername();
+    if (username.value === '') {
+      if (email.value.indexOf('@') !== -1) {
+        setUser({
+          ...user,
+          email: email.value,
+          username: email.value.split('@')[0],
+        });
+        console.log(user);
+      } else {
+        setUser({
+          ...user,
+          email: email.value,
+          username: '',
+        });
+        console.log(user);
+      }
+    } else {
+      setUser({
+        ...user,
+        email: email.value,
+        username: user.username,
+      });
+    }
+  };
+
+  console.log(user);
+
+  return (
+    <>
+      <h1>REGISTRATION</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email Address"
+          onChange={handleChangeUsername}
+          value={user.email}
+        />
+        <br />
+
+        <input
+          type="text"
+          id="username"
+          name="username"
+          placeholder="User Name"
+          onChange={handleChange}
+          value={user.username || ''}
+        />
+        <br />
+
+        <input
+          type="text"
+          id="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+          value={user.password}
+        />
+        <br />
+
+        <input
+          type="text"
+          id="password-confirm"
+          name="passwordConfirm"
+          placeholder="Confirm Password"
+          onChange={handleChange}
+          value={user.passwordConfirm}
+        />
+        <br />
+        <button type="submit">REGISTER</button>
+      </form>
+    </>
+  );
+};
+
+export default Registration;
